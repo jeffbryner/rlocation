1e53b600299568bdcc12645c6eef72955af56c55
Jeff Bryner <jeff@jeffbryner.com>
2025-06-15 15:31:31 -0700

    working but not permitted

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,96 @@ use libui::prelude::*;
 use public_ip_address::lookup::LookupProvider;
 //use std::net::{IpAddr, Ipv4Addr};
 
+use objc2::rc::Retained;
+use objc2::runtime::{AnyObject, NSObject, ProtocolObject};
+use objc2::{define_class, msg_send, AllocAnyThread};
+use objc2_core_location::{
+    CLAuthorizationStatus, CLLocation, CLLocationManager, CLLocationManagerDelegate,
+};
+use objc2_foundation::{MainThreadMarker, NSArray, NSObjectProtocol};
+use std::sync::RwLock;
+use once_cell::sync::Lazy;
+use log::{LevelFilter, Log, Metadata, Record};
+use oslog::OsLogger;
+
+#[derive(Debug, Copy, Clone, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct LastLocation {
+    pub latitude: f64,
+    pub longitude: f64,
+}
+
+
+pub static LAST_LOCATION: Lazy<RwLock<Option<LastLocation>>> =
+    Lazy::new(|| RwLock::new(None));
+
+
+// Define the MyLocationDelegate class
+define_class!(
+    #[name = "MyLocationDelegate"]
+    #[unsafe(super = NSObject)]
+    #[thread_kind = AllocAnyThread]
+    struct MyLocationDelegate;
+
+    unsafe impl CLLocationManagerDelegate for MyLocationDelegate {
+        #[unsafe(method(locationManager:didChangeAuthorizationStatus:))]
+        fn did_change_authorization(
+            &self,
+            _manager: &CLLocationManager,
+            status: CLAuthorizationStatus,
+        ) {
+            log::info!("Authorization status changed: {:?}", status);
+        }
+
+        #[unsafe(method(locationManager:didUpdateLocations:))]
+        fn did_update_locations(
+            &self,
+            _manager: &CLLocationManager,
+            locations: &NSArray<CLLocation>,
+        ) {
+            if let Some(loc) = locations.firstObject() {
+                let coord = unsafe { loc.coordinate() };
+                log::info!("Updated location: {}, {}", coord.latitude, coord.longitude);
+                let mut lock = LAST_LOCATION.write().unwrap();
+  
+                *lock = Some(LastLocation {
+                    latitude: coord.latitude,
+                    longitude: coord.longitude,
+                });
+            }
+        }
+
+        #[unsafe(method(locationManager:didFailWithError:))]
+        fn did_fail_with_error(&self, _manager: &CLLocationManager, _error: &AnyObject) {
+            log::error!("Location manager failed with error");
+        }
+    }
+
+    unsafe impl NSObjectProtocol for MyLocationDelegate {}
+);
+
+impl MyLocationDelegate {
+    fn new() -> Retained<Self> {
+        let this = Self::alloc();
+        unsafe { msg_send![this, init] }
+    }
+}
+
+// Global delegate holder
+static LOCATION_DELEGATE: Lazy<Retained<MyLocationDelegate>> =
+    Lazy::new(|| MyLocationDelegate::new());
+
+// Main-thread-only CLLocationManager holder
+static mut LOCATION_MANAGER: Option<Retained<CLLocationManager>> = None;
 
 fn main() {
+    OsLogger::new("com.example.test")
+        .level_filter(LevelFilter::Debug)
+        .category_level_filter("Settings", LevelFilter::Trace)
+        .init()
+        .unwrap();    
     embed_plist::embed_info_plist!("../Info.plist");    
+
     let ui = UI::init()
         .expect("Couldn't initialize UI library");
     
@@ -28,6 +115,17 @@ fn main() {
 
     layout.append(label, LayoutStrategy::Stretchy);    
 
+    let manager = unsafe { CLLocationManager::new() };
+
+    log::info!("Location manager started");
+
+    unsafe {
+        let delegate: &ProtocolObject<dyn CLLocationManagerDelegate> =
+            ProtocolObject::from_ref(&**LOCATION_DELEGATE);
+        manager.setDelegate(Some(delegate));
+        manager.requestAlwaysAuthorization();
+        manager.requestLocation();    
+    }
     win.set_child(layout);
     win.show();
     ui.main();
